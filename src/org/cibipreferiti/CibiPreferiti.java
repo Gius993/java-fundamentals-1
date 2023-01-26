4f2f9e5ccf6969d0cfdbf8764637bd09aed9abd8
Gius993 <giuseppevidili9@gmail.com>
2023-01-26 14:47:43 +0100

    cibi preferiti

---
+++ src/org/cibipreferiti/CibiPreferiti.java
@@ -0,0 +1,15 @@
+package org.lesson.cibipreferiti;
+
+
+
+public class CibiPreferiti {
+	public static void main(String [] args) {
+	
+		//array cibi
+		String[] cibiPreferiti = {"Capessante", "Calamari", "Manzo", "Verdure fritte", "Zampone"};
+		System.out.print(" lunghezza classifica " + cibiPreferiti.length);
+		System.out.print(" in prima posizione : " + cibiPreferiti[0]);
+		System.out.print(" in ultima posizione : " + cibiPreferiti[3]);
+		System.out.print(" in mezzo : " + cibiPreferiti[2]);
+	}
+}
